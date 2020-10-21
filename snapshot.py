50cf90245b227f512b67149af53ad9c778acef3e
Brian Aydemir <baydemir@morgridge.org>
2020-10-21 08:01:25 -0500

    Fix type checking under Python 3.6

--- snapshot.py
+++ snapshot.py
@@ -17,46 +17,46 @@ import json
 import logging
 import os
 import stat
+import sys
 from pathlib import Path
-from typing import Dict, Union
+from typing import Dict, NamedTuple, Union
 
 import docopt
 
-LOGGING_NAME = "snapshot"
-
 BUFFER_SIZE = 2 ** 20  # 1 MiB
 
-#
-# Type definitions.
-#
-
 FSPath = str
-AnyPath = Union[Path, FSPath]
-TypedDict = object  # TODO: Import from 'typing' with Python >= 3.8
+AnyPath = Union[FSPath, os.PathLike]
 
 
-class FileMetadata(TypedDict):
+class FileMetadata(NamedTuple):
     size: int
-    sha1_digest: str
+    digest: str
 
 
 FileManifest = Dict[FSPath, FileMetadata]
 
 
-class Snapshot(TypedDict):
+class Snapshot(NamedTuple):
     version: str
     root: FSPath
     manifest: FileManifest
 
 
-############################################################################
+class SnapshotDecoder(json.JSONDecoder):
+    def decode(self, s):
+        obj = super().decode(s)
+
+        obj[2] = {k: FileMetadata(*v) for k, v in obj[2].items()}
+
+        return Snapshot(*obj)
 
 
-def sha1(path: AnyPath, mode: int) -> str:
+def make_digest(path: AnyPath, mode: int) -> str:
     """
-    Computes the SHA-1 hash for the file at the given path.
+    Returns the SHA-1 hash for the file at the given path.
     """
-    hash = hashlib.sha1()
+    hasher = hashlib.sha1()
 
     if stat.S_ISREG(mode):
         with open(path, "rb") as file:
@@ -64,24 +64,18 @@ def sha1(path: AnyPath, mode: int) -> str:
                 data = file.read(BUFFER_SIZE)
                 if not data:
                     break
-                hash.update(data)
+                hasher.update(data)
     else:
-        message = f"Not a regular file: {path}"
+        raise RuntimeError(f"Not a regular file: {path}")
 
-        log = logging.getLogger(LOGGING_NAME)
-        log.error(message)
-
-        raise RuntimeError(message)
-
-    return hash.hexdigest()
+    return hasher.hexdigest()
 
 
 def make_manifest(root: AnyPath) -> FileManifest:
     """
-    Computes the file manifest for the given directory.
+    Returns the file manifest for the given directory.
     """
-    manifest: FileManifest = {}
-    log = logging.getLogger(LOGGING_NAME)
+    manifest = {}
 
     for (dirpath, dirnames, filenames) in os.walk(root):
         dirnames[:] = sorted(dirnames)
@@ -89,15 +83,15 @@ def make_manifest(root: AnyPath) -> FileManifest:
         for filename in sorted(filenames):
             path = Path(dirpath) / filename
 
-            log.info(f"PATH {path}")
+            logging.info(f"PATH {path}")
 
             st_info = path.lstat()
             rel_path = os.fspath(path.relative_to(root))
 
-            manifest[rel_path] = {
-                "size": st_info.st_size,
-                "sha1_digest": sha1(path, st_info.st_mode),
-            }
+            manifest[rel_path] = FileMetadata(
+                size=st_info.st_size, digest=make_digest(path, st_info.st_mode),
+            )
+
     return manifest
 
 
@@ -105,60 +99,64 @@ def compare_snapshots(s1: Snapshot, s2: Snapshot) -> None:
     """
     Prints any differences found between the two given snapshots.
     """
-    r1 = s1["root"]
-    r2 = s2["root"]
+    r1 = s1.root
+    r2 = s2.root
 
     if r1 == r2:
         r1 += "@1"
         r2 += "@2"
 
-    m1 = s1["manifest"]
-    m2 = s2["manifest"]
+    m1 = s1.manifest
+    m2 = s2.manifest
 
     paths1 = set(m1)
     paths2 = set(m2)
 
     for path in paths1 - paths2:
-        print(f"In '{r1}' but not '{r2}': {path}")
+        print(f"Only in '{r1}': {path}")
     for path in paths2 - paths1:
-        print(f"In '{r2}' but not '{r1}': {path}")
+        print(f"Only in '{r2}': {path}")
 
     for path in paths1 & paths2:
         if m1[path] != m2[path]:
             print(f"Files differ: {path}")
 
 
+############################################################################
+# Command-line interface.
+
+
+def init_logging() -> None:
+    logging.basicConfig(format="[%(asctime)s] %(levelname)s %(message)s")
+
+
 def main() -> None:
-    """
-    Main entry point, when called as a command-line utility.
-    """
     args = docopt.docopt(__doc__)
 
     if args["--verbose"]:
-        logging.basicConfig(
-            level=logging.DEBUG, format="[%(asctime)s] %(levelname)s %(name)s %(message)s",
-        )
+        logging.getLogger().setLevel(logging.DEBUG)
 
     if args["make"]:
         dir = args["<dir>"]
 
         root = os.fspath(Path(dir).resolve())
 
-        snapshot: Snapshot = {
-            "version": "2",
-            "root": root,
-            "manifest": make_manifest(root),
-        }
+        snapshot = Snapshot(version="4", root=root, manifest=make_manifest(root))
 
         print(json.dumps(snapshot, indent=2, sort_keys=True))
 
     if args["compare"]:
         with open(args["<s1>"]) as file:
-            s1 = json.load(file)
+            s1 = json.load(file, cls=SnapshotDecoder)
         with open(args["<s2>"]) as file:
-            s2 = json.load(file)
+            s2 = json.load(file, cls=SnapshotDecoder)
         compare_snapshots(s1, s2)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        init_logging()
+        main()
+    except Exception as exn:
+        logging.exception(f"Exception: {exn}")
+        sys.exit(1)
